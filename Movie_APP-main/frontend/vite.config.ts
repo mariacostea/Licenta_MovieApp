52bfbe583c62e298094c67e54b15443c5e336f37
Maria Costea <maria03costea@email.com>
2025-06-21 23:00:35 +0300

    Add Docker and docker-compose setup for backend and frontend deployment6

--- Movie_APP-main/frontend/vite.config.ts
+++ Movie_APP-main/frontend/vite.config.ts
@@ -2,18 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 4173,
-    allowedHosts: ['licenta-frontend-r2gn.onrender.com']
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://licenta-backend-nf1m.onrender.com',
-        changeOrigin: true,
-        secure: false
+    plugins: [react()],
+    preview: {
+      host: '0.0.0.0', // ⬅️ acest rând lipsește la tine
+      port: 4173,
+      allowedHosts: ['licenta-frontend-r2gn.onrender.com']
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://licenta-backend-nf1m.onrender.com',
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
-  }
-});
+  });
+
