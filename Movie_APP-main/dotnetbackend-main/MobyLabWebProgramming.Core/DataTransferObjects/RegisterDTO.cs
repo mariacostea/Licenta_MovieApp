8fec04d25ee1c3c789989f149dff3231a5db7e1d
Maria Costea <maria03costea@email.com>
2025-06-28 16:43:40 +0300

    frontend_profilepic9

--- Movie_APP-main/dotnetbackend-main/MobyLabWebProgramming.Core/DataTransferObjects/RegisterDTO.cs
+++ Movie_APP-main/dotnetbackend-main/MobyLabWebProgramming.Core/DataTransferObjects/RegisterDTO.cs
@@ -5,6 +5,6 @@
         public string Username { get; set; } = default!;
         public string Email { get; set; } = default!;
         public string Password { get; set; } = default!;
-        public string ProfilePictureUrl { get; set; } = default!;
+        public string? ProfilePictureUrl { get; set; } = default!;
     }
 }
